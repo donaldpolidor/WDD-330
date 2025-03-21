c64573bbc017d593c4291ba0de21888ed0cba563
do-3 <donaldpolidor30@github.com>
2025-03-21 11:31:54 -0400

    commit message

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,10 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+// Retrieves a specific parameter from the URL
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
